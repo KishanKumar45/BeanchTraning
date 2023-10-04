926786d7ec61d8b622a88f5e277efa2ec2c3cb0a
KishanKumar45 <130962209+KishanKumar45@users.noreply.github.com>
2023-10-04 22:05:22 +0530

    Update SampleTestCase.java

--- src/main/java/jenkinsMavenIntegration/SampleTestCase.java
+++ src/main/java/jenkinsMavenIntegration/SampleTestCase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
 public class SampleTestCase {
 @Test
 public void Testgoogle() throws InterruptedException {
